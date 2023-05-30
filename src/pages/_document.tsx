cd89b394d966164685abb91841d7d312e25224bb
Mateus Felipe Guedes Gava <78923115+prgMgava@users.noreply.github.com>
2023-05-30 19:35:37 -0300

    config: add tailwind config (#1)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,11 +10,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,500;0,800;1,100;1,300;1,500;1,800&family=Roboto+Slab:wght@200;400;800&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />
         <NextScript />
-        teste
       </body>
     </Html>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Pokedex
+      <span className="text-3xl font-bold underline">Pokedex</span>
       {/* TODO: genarate favicons, and update manifest.json */}
     </>
   );
